2c7065fa8c87b68c186f4ac7795643bac1c14d97
bytiks <saadkhan7862254j@gmail.com>
2025-06-28 03:48:00 +0530

    ServletFirstDemo

---
+++ ServletFirstDemo/src/main/java/in/sp/backend/MyServlet.java
@@ -0,0 +1,22 @@
+package in.sp.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter printWriter = response.getWriter();
+		printWriter.print("Saif ALI khan");
+	}
+}
